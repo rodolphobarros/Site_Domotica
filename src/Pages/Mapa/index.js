c8834abcc7a7305bb525b18f01eb1613a72aba3b
rodolphobarros@gmail.com <rodolphobarros@gmail.com>
2020-06-01 11:03:49 +0100

    versao v1.1

--- src/Pages/Mapa/index.js
+++ src/Pages/Mapa/index.js
@@ -11,33 +11,32 @@ function Mapa()
                     <p>Gerenciar sua casa</p>
                 </div>
 
-                <form className="grupo" action='/Controlo'>
-            
+                <form className="grupo" action='/_Sala_E'>
                     <button type="submit">Sala de Estar</button>
                 </form>
 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>Sala de Jantar</button>
+                <form className="grupo" action='/_Sala_J'>
+                    <button type="submit" >Sala de Jantar</button>
                 </form>
 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>Cozinha</button>
+                <form className="grupo" action='/Cozinha'>
+                    <button type="submit">Cozinha</button>
                 </form>
 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>Varanda</button>
+                <form className="grupo" action='_A_Servico'>
+                    <button type="submit">Área de Serviço</button>
                 </form>
 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>WC Social</button>
+                <form className="grupo" action='/_CB_1'>
+                    <button type="submit">Casa de Banho</button>
                 </form>
                 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>Quarto Master</button>
+                <form className="grupo" action='/_Quarto_s'>
+                    <button type="submit">Quarto Master</button>
                 </form>
 
-                <form className="grupo" action='/Controlo'>
-                    <button type="submit" action='/Controlo'>WC QMaster</button>
+                <form className="grupo" action='/_CB_S'>
+                    <button type="submit">Casa de Banho Q.Master</button>
                    
                 </form>
               
--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,15 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Log from './Pages/Log';
 import Mapa from './Pages/Mapa';
 import Controlo from './Pages/Controlo';
+import _A_Servico from './Pages/_A_Servico';
+import _CB_1 from './Pages/_CB_1';
+import _CB_S from './Pages/_CB_S';
+import _Cozinha from './Pages/_Cozinha';
+import _Quarto_1 from './Pages/_Quarto_1';
+import _Quarto_S from './Pages/_Quarto_S';
+import _Sala_E from './Pages/_Sala_E';
+import _Sala_J from './Pages/_Sala_J';
+
 
     function Routes() {
         return(
@@ -13,8 +22,14 @@ import Controlo from './Pages/Controlo';
                     <Route path="/" exact component={Log} />
                     <Route path="/Controlo" component={Controlo} />
                     <Route id='mapa'path="/Mapa" component={Mapa} />
-
-                    
+                    <Route id=''path="/_A_Servico" component={_A_Servico} />
+                    <Route id=''path="/_CB_1" component={_CB_1} />
+                    <Route id=''path="/_CB_S" component={_CB_S} />
+                    <Route id=''path="/_Cozinha" component={_Cozinha} />
+                    <Route id=''path="/_Quarto_1" component={_Quarto_1} />
+                    <Route id=''path="/_Quarto_S" component={_Quarto_S} />
+                    <Route id=''path="/_Sala_E" component={_Sala_E} />
+                    <Route id=''path="/_Sala_J" component={_Sala_J} />
 
                 </Switch>
             
